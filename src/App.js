aeb5f5bab505906c1d614900a2f8fa606261745f
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-11-30 22:07:47 +0100

    create header component and data.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Header from './Components/Header/Header';
 
 export default function App() {
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'af74ccd02cmshca430382404f56ep117880jsnb78ef35251b6',
+        'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://asos2.p.rapidapi.com/products/v3/detail?id=9851612&lang=en-NG&store=NG&sizeSchema=NG&currency=NGN', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }, [])
+
   return (
-    <div>
-      <h2 className='text-3xl font-bold underline'>My name is Abidoye Abiodun</h2>
+    <div className='bg-yellow-400'>
+      <Header/>
     </div>
   )
 }
