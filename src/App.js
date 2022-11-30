dbeb74e7e5a719729d03faba0c697e57b8fc81da
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-11-30 17:12:41 +0100

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold underline'>My name is Abidoye Abiodun</h2>
+    </div>
+  )
+}
 
